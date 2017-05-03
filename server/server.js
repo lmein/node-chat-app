87f1159e5ed35cdfa0bdb863ef09d6f01a740dfd
lmein <lmeinecke@gmail.com>
2017-05-03 15:05:38 -0500

    Limit location message to only in same room.

--- server/server.js
+++ server/server.js
@@ -70,8 +70,13 @@ io.on('connection', (socket) => {
 
   //socket.on listens for event.
   socket.on('createMessage', (message, callback) => {
-    console.log('createMessage', message);
-    io.emit('newMessage', generateMessage(message.from, message.text));
+    //console.log('createMessage', message);
+    var user = users.getUser(socket.id);
+
+    if (user && isRealString(message.text)) {
+      io.to(user.room).emit('newMessage', generateMessage(user.name, message.text));
+    }
+
     callback();
     // io.emit('newMessage', {
     //   from: message.from,
@@ -87,8 +92,12 @@ io.on('connection', (socket) => {
   });
 
   socket.on('createLocationMessage', (coords) => {
+    var user = users.getUser(socket.id);
+
+    if (user) {
+      io.to(user.room).emit('newLocationMessage', generateLocationMessage(`${user.name}'s location is: `, coords.latitude, coords.longitude))
+    }
     //io.emit('newMessage', generateMessage('Admin', `${coords.latitude}, ${coords.longitude}`))
-    io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude))
   });
 
   socket.on('disconnect', () => {
